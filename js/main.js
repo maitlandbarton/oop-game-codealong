dc0d3d402be0fb1b73aaf6e3e81cdb9ec24c4737
Mattie Barton <maitlandbarton@gmail.com>
2023-03-02 11:58:37 +0100

    functionality to display and move player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log("hello world!");
+
+
+class Player {
+    constructor(){
+        this.positionX = 0;
+        this.positionY = 0;
+        this.playerElm = document.getElementById("player");
+    }
+    moveLeft(){ // want to modify the position of the player
+        this.positionX--;
+        this.playerElm.style.left = this.positionX + 'vw';
+    }
+    moveRight(){
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + 'vw';
+    }
+}
+
+
+const myPlayer = new Player();
+myPlayer.moveRight();
+
+
+//attach event listeners
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft"){
+        myPlayer.moveLeft();
+    } else if (e.key === "ArrowRight"){
+        myPlayer.moveRight();
+    }
+});
